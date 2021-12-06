8e545fcfa311131307cef6ea078be2a18fd34dff
kckhafra <kckhafra@gmail.com>
2021-12-06 17:27:23 -0500

    trying

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -114,8 +114,8 @@ def trade():
         # TODO: Fill the order
         verifyer = check_sig(payload,signature)
         if(verifyer):
-            order = Order(receiver_pk=receiver,sender_pk=senderPubKey,buy_currency=buyCurrency,sell_currency=sellCurrency,buy_amount=buyAmount,sell_amount=sellAmount)
-            g.session.add(order)
+            orderV = Order(receiver_pk=receiver,sender_pk=senderPubKey,buy_currency=buyCurrency,sell_currency=sellCurrency,buy_amount=buyAmount,sell_amount=sellAmount)
+            g.session.add(orderV)
             g.session.commit()
             return jsonify(True)
         else:
