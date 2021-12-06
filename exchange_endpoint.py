4d710cd2e994965cdf6cef52017bbe697d460070
kckhafra <kckhafra@gmail.com>
2021-12-06 17:17:59 -0500

    trying

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -39,13 +39,13 @@ def check_sig(payload,signature):
         # eth_account.Account.enable_unaudited_hdwallet_features()
         # acct, mnemonic = eth_account.Account.create_with_mnemonic()
         senderPubKey = payload['sender_pk']
-        # p=json.dumps(payload)
+        p=json.dumps(payload)
         eth_pk = senderPubKey
         eth_sk = signature
         # eth_pk = acct.address
         # eth_sk = acct.key
 
-        eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
+        eth_encoded_msg = eth_account.messages.encode_defunct(text=p)
         eth_sig_obj = eth_account.Account.sign_message(eth_encoded_msg,eth_sk)
         if eth_account.Account.recover_message(eth_encoded_msg,signature=eth_sig_obj.signature.hex()) == eth_pk:
             return True
@@ -114,11 +114,13 @@ def trade():
         # TODO: Fill the order
         verifyer = check_sig(payload,signature)
         if(verifyer):
+            print("Receiver",receiver)
             order = Order(receiver_pk=receiver,sender_pk=senderPubKey,buy_currency=buyCurrency,sell_currency=sellCurrency,buy_amount=buyAmount,sell_amount=sellAmount)
             g.session.add(order)
             g.session.commit()
             return jsonify(True)
         else:
+            log_message(content)
             return jsonify(False)
         
         # TODO: Be sure to return jsonify(True) or jsonify(False) depending on if the method was successful
